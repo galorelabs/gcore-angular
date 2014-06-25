177375a074639eaae3408f35e550ebd6cccace20
Ardee Aram <ardee.aram@galoretv.com>
2014-06-25 13:12:25 +0800

    Added generic GET interface

--- gcore-angular.js
+++ gcore-angular.js
@@ -5,30 +5,155 @@
 
 angular.module('gcore', ['ngCookies'])
 
+.provider('$gcore', function() {
 
-.factory('gcore', function($cookies, $cookieStore, $http) {
+	/** Configurable Properties **/
+    this.endpoint = 'https://api.gcore.galoretv.com';
+    this.cookie_name = 'gcore_authentication';
+    this.user_cookie_name = 'gcore_user';
+    
+    this.set_endpoint = function(endpoint) {
+        this.endpoint = endpoint;
+    };
+    
+    this.set_cookie_name = function(cookie_name) {
+    	this.cookie_name = cookie_name;
+    };
+    
+    this.set_user_cookie_name = function(user_cookie_name) {
+    	this.user_cookie_name = user_cookie_name;
+    };    
+
+    /*** Actual object to return **/
+    this.$get = function($cookies, $cookieStore, $http) {
+        
+    	/** objects to form closure with **/
+    	var endpoint = this.endpoint;
+    	var cookie_name = this.cookie_name;
+    	var user_cookie_name = this.user_cookie_name;
+        
+        return {
+        	
+        	/*** Utilities and Helpers ***/
+        	
+        	endpoint: function() {
+        		return endpoint;
+        	},
+        	
+    		get_request_headers: function() {
+			  var gcore_authentication = $cookieStore.get(cookie_name);
+			  return {
+		    		  Authorization : gcore_authentication, 
+		    		  Accept : 'application/json',
+		    		  "Content-Type": 'application/json'
+		      };		
+    		},
+    		
+    		/** Cookie-related functions **/
+    		
+    		is_logged_in: function() {
+    			if($cookieStore.get(cookie_name) != null) {
+    				return true;
+    			} else {
+    				return false;
+    			}   			
+    		},
+    		
+    		get_logged_on_user: function() {
+    			return $cookieStore.get(user_cookie_name);
+    		},
+    		
+    		/** TODO: Connect with DELETE /session **/
+    		logout: function() {
+    			$cookieStore.remove(cookie_name);
+    			$cookieStore.remove(user_cookie_name);
+    		},
+    		
+    		/** Synactic sugars (API Alias) **/
+
+
+    		login : function(username, password) {
+    			return this.post_session(username, password);
+    		}, 
+    		
+        	
+        	/******* API ******/
+ 
+    		/*** Session ***/
+    		post_session: function(username, password) {
+    			return $http.post(
+    				endpoint + "/session", 
+    				JSON.stringify({username: username, password: password})
+    			)	
+    			.success(function(data, status, headers, config) {
+
+    				//create session
+    				user_id = data.model.user.id;
+    				user_secret_token = data.model.user.secret_token;
+    				$cookieStore.put(cookie_name, user_id + ":" + user_secret_token);
+    				$cookieStore.put(user_cookie_name, data.model.user);
+    			
+    			});
+    		},
+    		
+    		/*** Orders ***/
+    		
+    		list_orders: function(store) {
+    			return $http.get(
+    				endpoint + "/stores/" + store + "/sales_orders"	
+    			);
+    			
+    		}
+    		
+        };
+    };
+
+
+})
+
+;
+
+//hey, we can configure a provider!            
+//.config(function($gcoreProvider){
+//	$gcoreProvider.setEndpoint('http://qa.api.gcore.galoretv.com');
+//});
+
+
+//.factory('$gcore', function($cookies, $cookieStore, $http) {
 /***
  * This is a thin wrapper for the GCORE REST API. 
  */	
 
 
-	var endpoint = 'http://api.gcore.galoretv.com';
+//	var live_endpoint = 'http://qa.api.gcore.galoretv.com';
+//	var qa_endpoint = 'http://api.gcore.galoretv.com';
 
-	var self = {
+//	var self = {
 
-		/** Properties **/
-		endpoint : endpoint,
+		/** Properties **
 		
-		/** Methods: Session **/
+		environment: "live",
+			
+			
+		endpoint : function() {
+			if (self.environment == "live") {
+				return live_endpoint;
+			} else {
+				return qa_endpoint;
+			}
+		},
+		
+		/** Methods: Session **
+		
 
-		/** Synactic sugars **/
+		/** Synactic sugars **
 
 
 		login : function(username, password) {
 			return this.post_session(username, password);
 		},
 		
-		/*** Utilities and Helpers ***/
+		/*** Utilities and Helpers ***
 		
 		default_error_callback: function(data, status, headers, config) {
 			console.log(data.message);
@@ -44,9 +169,9 @@ angular.module('gcore', ['ngCookies'])
 	     };		
 		},
 	
-		/******* API ******/
+		/******* API ******
 		
-		/** POST /session **/
+		/** POST /session **
 		post_session: function(username, password) {
 			return $http.post(
 				endpoint + "/session", 
@@ -68,12 +193,12 @@ angular.module('gcore', ['ngCookies'])
 		},
 
 		
-		/** DELETE /session **/
+		/** DELETE /session **
 		delete_session: function() {
 		},
 		
 		
-		/********* POST systems/email **********************************/
+		/********* POST systems/email **********************************
 		post_systems_email: function(to, subject, body) {
 			return $http.post(
 				endpoint + '/systems/email',
@@ -90,5 +215,6 @@ angular.module('gcore', ['ngCookies'])
 	};
 
 	return self;
+	*/
 
-});
+//});
