4d7d1cedbd11b70b74fc0b1868fb61c3555ff544
Ardee Aram <ardee.aram@galoretv.com>
2014-06-25 13:36:40 +0800

    Extracted from gcore.galoretv.com

--- gcore-angular.js
+++ gcore-angular.js
@@ -32,7 +32,7 @@ angular.module('gcore', ['ngCookies'])
     	var cookie_name = this.cookie_name;
     	var user_cookie_name = this.user_cookie_name;
         
-        return {
+        var self = {
         	
         	/*** Utilities and Helpers ***/
         	
@@ -47,6 +47,38 @@ angular.module('gcore', ['ngCookies'])
 		    		  Accept : 'application/json',
 		    		  "Content-Type": 'application/json'
 		      };		
+    		},
+    		
+    		url_params: function(hash) {
+    			
+    			if(Object.getOwnPropertyNames(hash).length !== 0) {
+    				url = "?";
+    				angular.forEach(hash, function(value, key){
+    					url += key + "=" + value + "&";
+    				});
+    				url = url.substring(0, url.length - 1);
+    			} else {
+    				url = "";
+    			}  			
+    			return url;
+    		},
+    		
+    		order_status: function() {
+    			return [
+    			        'canceled',
+    			        'closed',
+    			        'delivered',
+    			        'for_verification',
+    			        'invoiced',
+    			        'new',
+    			        'not_delivered', 
+    			        'on_hold',
+    			        'packed',
+    			        'pending',
+    			        'return_received',
+    			        'shipped',
+    			        'verified'    			        
+    			];
     		},
     		
     		/** Cookie-related functions **/
@@ -74,16 +106,46 @@ angular.module('gcore', ['ngCookies'])
 
     		login : function(username, password) {
     			return this.post_session(username, password);
-    		}, 
+    		},
+    		
+    		
+    		/*** Generic API ***/
+    		
+    		
+    		get: function(resource) {
+    			return $http.get(
+    				self.endpoint + resource,
+    				{headers: self.get_request_headers()}
+    			);
+    		},    		
     		
         	
-        	/******* API ******/
+        	/******* API 
+        	 * 
+        	 * Naming convention:
+        	 * 
+        	 * Assume that we have resource named "resources", we follow Rails naming convention at
+        	 * http://guides.rubyonrails.org/routing.html,
+        	 * except for "index", which becomes "list".
+        	 * 
+        	 * Any additional parameters will be included in the "params" hash. The "params" hash is optional.
+        	 * 
+        	 * 
+        	 * GET /resources 			-> $gcore.list_resources(params);
+        	 * POST /resources 			-> $gcore.create_resources(params);
+        	 * GET /resources/:id 		-> $gcore.show_resources(id, params);
+        	 * PUT /resources/:id 		-> $gcore.update_resources(id, params);
+        	 * PUT /resources			-> $gcore.mass_update_resources(params);
+        	 * DELETE /resources/:id 	-> $gcore.destroy_resources(id, params);
+        	 * 
+        	 * ******/
  
     		/*** Session ***/
     		post_session: function(username, password) {
     			return $http.post(
     				endpoint + "/session", 
-    				JSON.stringify({username: username, password: password})
+    				JSON.stringify({username: username, password: password}),
+    				{headers: self.get_request_headers()}
     			)	
     			.success(function(data, status, headers, config) {
 
@@ -97,15 +159,116 @@ angular.module('gcore', ['ngCookies'])
     		},
     		
     		/*** Orders ***/
-    		
-    		list_orders: function(store) {
+    		list_orders: function(params) {
+    			
+    			
+    			var url = endpoint + "/sales_orders"; 
+    			
+    			url += self.url_params(params);
+
     			return $http.get(
-    				endpoint + "/stores/" + store + "/sales_orders"	
+    				url,
+    				{headers: self.get_request_headers()}
     			);
     			
+    		},
+    		
+    		show_orders: function(id, params) {
+    			
+    			var url = endpoint + "/sales_orders/" + id;
+    			
+    			if (params) {
+    				url += self.url_params(params);
+    			}
+    			    			
+    			return $http.get(
+        				url,
+        				{headers: self.get_request_headers()}
+        			);
+    		},
+    		
+    		update_orders: function(id, params) {
+    			
+    			var url = endpoint + "/sales_orders/" + id;
+    			
+    			return $http.put(
+    					url,
+    					params['_json'] ? JSON.stringify(params['_json'])  : JSON.stringify(params),
+    					{headers: self.get_request_headers()}
+    			);
+    			
+    			
+    		},
+    		
+    		mass_update_orders: function(params) {
+ 
+    			var url = endpoint + "/sales_orders";
+    			
+    			return $http.put(
+    					url,
+    					params['_json'] ? JSON.stringify(params['_json'])  : JSON.stringify(params),
+    					{headers: self.get_request_headers()} 
+    			);		
+    		},
+    		
+    		
+    		/*** Products ***/
+    		list_products: function(params) {
+    			
+    			
+    			var url = endpoint + "/stores/" + params['stores'] + "/products"; 
+    			delete params['stores'];
+    			
+    			url += self.url_params(params);
+
+    			return $http.get(
+    				url,
+    				{headers: self.get_request_headers()}
+    			);
+    			
+    		},
+    		
+    		/*** Catalog Products ***/
+    		
+    		
+    		
+    		/*** Comments ***/
+    		
+    		delete_comments: function(id, params) {
+    			
+    			var url = endpoint + "/sales_orders/" + params['sales_orders_id'] + "/comments/" + id; 
+    			delete params['sales_orders_id'];
+    			
+//    			return $http.delete(
+//    					url,
+//    					params['_json'] ? JSON.stringify(params['_json'])  : JSON.stringify(params),
+//    					{headers: self.get_request_headers()}
+//    			);
+    			
+    			
+    		},
+    		
+    		/*** Systems ***/
+    		create_email: function(params) {
+    			
+    			var url = endpoint + "/systems/email";
+    			
+    			return $http.post(
+    					url,
+    					params['_json'] ? JSON.stringify(params['_json']) : JSON.stringify(params),
+    					{headers: self.get_request_headers()}
+    			);   			    			
+    		},
+    		
+    		//Syntactic sugar is goodd for the brain
+    		send_email: function(params) {
+    			return this.create_email(params);
     		}
     		
+    		
         };
+        
+        return self;
     };
 
 
